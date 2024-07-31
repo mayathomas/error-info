e839bac00f0b350158151a658c94cb877b0d158c
maya <770257551@qq.com>
2024-07-31 11:26:16 +0800

    features: support ToErrorInfo Macro

--- error-code/src/lib.rs
+++ error-code/src/lib.rs
@@ -1,11 +1,35 @@
+use core::fmt;
+pub use error_code_derive::ToErrorInfo;
+use std::str::FromStr;
+
+#[derive(Debug)]
 pub struct ErrorInfo<T> {
-    pub app_code: T,  //could be HTTP 400 bad request
-    pub code: String, //something like "01E739"
-    pub client_msg: String,
+    pub app_code: T,        //could be HTTP 400 bad request
+    pub code: &'static str, //something like "01E739"
+    pub client_msg: &'static str,
     pub server_msg: String,
 }
 
 pub trait ToErrorInfo {
-    type T;
-    fn to_error_info(&self) -> ErrorInfo<Self::T>;
+    type T: FromStr;
+    fn to_error_info(&self) -> Result<ErrorInfo<Self::T>, <Self::T as FromStr>::Err>;
+}
+
+impl<T> ErrorInfo<T>
+where
+    T: FromStr,
+{
+    pub fn try_new(
+        app_code: &str,
+        code: &'static str,
+        client_msg: &'static str,
+        server_msg: impl fmt::Display,
+    ) -> Result<Self, T::Err> {
+        Ok(Self {
+            app_code: T::from_str(app_code)?,
+            code,
+            client_msg,
+            server_msg: server_msg.to_string(),
+        })
+    }
 }
